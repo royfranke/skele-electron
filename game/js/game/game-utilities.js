e8b6d69dc30b8e2c3d0d49f05ee9fd1773b1f6f3
royfranke <99510038+royfranke@users.noreply.github.com>
2025-12-13 10:35:19 -0500

    Made buildFacadeSection more lenient

--- game/js/game/game-utilities.js
+++ game/js/game/game-utilities.js
@@ -50,22 +50,55 @@ export default class GameUtilities {
 
     buildFacadeSection(_x, _y, width, height, material, layer) {
         /// Lower left is _x, _y
-        layer.weightedRandomize(material.LOWER_LEFT_, _x, _y, 1, 1);
+        if (material == undefined) {
+            console.warn('No material defined for facade at '+_x+', '+_y);
+            console.warn(material);
+            return;
+        }
+        var position = 'LOWER_LEFT_';
+        if (material[position] == undefined) {
+            position = 'LOWER_';
+        }
+        layer.weightedRandomize(material[position], _x, _y, width - 2, 1);
+
         layer.weightedRandomize(material.LOWER_, _x + 1, _y, width - 2, 1);
-        layer.weightedRandomize(material.LOWER_RIGHT_, _x + width - 1, _y, 1, 1);
+
+        position = 'LOWER_RIGHT_';
+        if (material[position] == undefined) {
+            position = 'LOWER_';
+        }
+
+        layer.weightedRandomize(material[position], _x + width - 1, _y, 1, 1);
 
         if (height > 2) {
             // go up by the number of mid tiles (height-2)
             _y = _y - (height - 2);
-            layer.weightedRandomize(material.MID_LEFT_, _x, _y, 1, height - 2);
+            var position = 'MID_LEFT_';
+            if (material[position] == undefined) {
+                position = 'MID_';
+            }
+            layer.weightedRandomize(material[position], _x, _y, 1, height - 2);
             layer.weightedRandomize(material.MID_, _x + 1, _y, width - 2, height - 2);
-            layer.weightedRandomize(material.MID_RIGHT_, _x + width - 1, _y, 1, height - 2);
+
+            position = 'MID_RIGHT_';
+            if (material[position] == undefined) {
+                position = 'MID_';
+            }
+            layer.weightedRandomize(material[position], _x + width - 1, _y, 1, height - 2);
         }
         if (height > 1) {
             _y = _y - 1;
-            layer.weightedRandomize(material.TOP_LEFT_, _x, _y, 1, 1);
+            var position = 'TOP_LEFT_';
+            if (material[position] == undefined) {
+                position = 'TOP_';
+            }
+            layer.weightedRandomize(material[position], _x, _y, 1, 1);
             layer.weightedRandomize(material.TOP_, _x + 1, _y, width - 2, 1);
-            layer.weightedRandomize(material.TOP_RIGHT_, _x + width - 1, _y, 1, 1);
+            position = 'TOP_RIGHT_';
+            if (material[position] == undefined) {
+                position = 'TOP_';
+            }
+            layer.weightedRandomize(material[position], _x + width - 1, _y, 1, 1);
         }
 
     }
